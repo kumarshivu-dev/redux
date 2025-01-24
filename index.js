0a24ca6e62f594536607d304dc4d6b0e207aafb0
Shivam Kumar <kumarshivu.dev@gmail.com>
2025-01-24 12:32:33 +0530

    chapter-2 | learn mutation vs non-mutation

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-console.log("React-Redux");
+/* 
+Mutation :- Changing or modifying an object/array directly, which affects the original data.
+*/
+// let state = {
+//   name: "shivam",
+//   age: 22,
+//   post: 0,
+// };
+
+// let newState = state;
+
+// console.log(state === newState);
+
+// state.name = "shiva";
+// newState.post++;
+
+// console.log(state === newState);
+
+// console.log("state: ", state);
+// console.log("newState: ", newState);
+
+/*
+React/Redux expects immutability to track changes. If the original object is mutated:
+1. React might not detect updates and re-render components incorrectly.
+2. Redux will fail to track state changes for debugging and history.
+*/
+
+/*
+Non-Mutation :- Creating a new object/array instead of directly modifying the existing one.
+*/
+let state = {
+  name: "shivam",
+  age: 22,
+  post: 0,
+};
+
+let newState = state;
+
+console.log(state === newState);
+
+state = { ...state, name: "shiva" };
+newState = { ...newState, post: newState.post + 1 };
+
+console.log(state === newState);
+
+console.log("state: ", state);
+console.log("newState: ", newState);
+
+/* 
+Immutability ensures React and Redux can properly track changes, improving performance and predictability.
+*/
